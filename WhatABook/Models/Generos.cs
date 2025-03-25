a4c933e42a9a340693872e24ce52e78c5542c223
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-25 00:43:53 -0400

    Modelo Generos

---
+++ WhatABook/Models/Generos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhatABook.Models;
+
+public class Generos
+{
+    [Key]
+    public int GeneroId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public string TipoGeneros { get; set; }
+
+    public List<Libros> Libros { get; set; } = new List<Libros>();
+
+}
