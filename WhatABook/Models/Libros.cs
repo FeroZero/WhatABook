430335cfcfc9aa757895ee08e03c517cad5699fd
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-25 00:42:04 -0400

    Modelo Libros

---
+++ WhatABook/Models/Libros.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhatABook.Models;
+
+public class Libros
+{
+    [Key]
+
+    public int LibroId { get; set; }
+
+    [RegularExpression(@"[A-Za-z\s]+$", ErrorMessage = "No se permiten caracteres especiales o numeros")]
+    [Required(ErrorMessage = " Campo obligatorio")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"[A-Za-z\s]+$", ErrorMessage = "No se permiten caracteres especiales o numeros")]
+    public string Autores { get; set; }
+
+    [Required(ErrorMessage = " Campo obligatorio")]
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public DateTime FechaPublicacion { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public string ImagenUrl { get; set; }
+
+    public virtual ICollection<Generos> Generos { get; set; } = new List<Generos>();
+
+
+}
+
