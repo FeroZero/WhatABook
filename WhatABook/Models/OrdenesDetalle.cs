323fc9e8e459243858c6dbef23b52500cac1ca7a
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-25 02:07:18 -0400

    detalle de ordenes creado.

---
+++ WhatABook/Models/OrdenesDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhatABook.Models
+{
+	public class OrdenesDetalle
+	{
+		[Key]
+		public int DetalleId { get; set; }
+
+		public int UsuarioId { get; set; }
+
+		public int LibroId { get; set; }
+
+		
+	}
+}
