68cb4dc704c9d425a7ae18e6e495555c5c569c99
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-25 02:06:59 -0400

    modelo de ordenes creado.

---
+++ WhatABook/Models/Ordenes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WhatABook.Models
+{
+	public class Ordenes
+	{
+		[Key]
+		public int OrdenId { get; set; }
+
+		[ForeignKey("Usuario")]
+		public int UsuarioId { get; set; }
+
+		public ICollection<OrdenesDetalle> ordenes { get; set; } = new List<OrdenesDetalle>();
+	}
+}
