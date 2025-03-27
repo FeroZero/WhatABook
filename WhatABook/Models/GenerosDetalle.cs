83f3d65e417bfdb876a58d8d8842c5b88431a303
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-27 04:26:37 -0400

    Se agrego el detalle de generos.

---
+++ WhatABook/Models/GenerosDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WhatABook.Models
+{
+	public class GenerosDetalle
+	{
+		[Key]
+		public int DetalleId { get; set; }
+
+		[ForeignKey("Libros")]
+		public int LibroId { get; set; }
+		public Libros Libros { get; set; } = new Libros();
+		
+		[ForeignKey("Generos")]
+		public int GeneroId { get; set; }
+		public Generos Generos { get; set; } = new Generos();
+
+	}
+}
