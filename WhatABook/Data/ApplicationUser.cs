26b292f5d92cf6b2c25a26260844f0fbcccdcdc2
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-27 04:27:11 -0400

    Se agrego datos a aplicationUser.

--- WhatABook/Data/ApplicationUser.cs
+++ WhatABook/Data/ApplicationUser.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WhatABook.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-    }
+		public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+		public string Direccion { get; set; }
+
+	}
 
 }
