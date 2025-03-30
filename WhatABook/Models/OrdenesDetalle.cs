b9436aa7a72ed3d3c4077dc5792926a6a69d7e06
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-30 12:41:36 -0400

    Se modificaron llaves en ordendetalles.

--- WhatABook/Models/OrdenesDetalle.cs
+++ WhatABook/Models/OrdenesDetalle.cs
@@ -1,21 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WhatABook.Models
+namespace WhatABook.Models;
+
+public class OrdenesDetalle
 {
-	public class OrdenesDetalle
-	{
-		[Key]
-		public int DetalleId { get; set; }
-		public int Cantidad { get; set; }
-		public double Monto { get; set; }
+	[Key]
+	public int DetalleId { get; set; }
+	public int Cantidad { get; set; }
+	public double Monto { get; set; }
 
-		[ForeignKey("Ordenes")]
-		public int OrdenId{ get; set; }
-		public Ordenes ordenes { get; set; }
+	[ForeignKey("Ordenes")]
+	public int OrdenId{ get; set; }
+	public Ordenes ordenes { get; set; }
 
-		[ForeignKey("Libros")]
-		public int LibroId { get; set; }
-		public Libros libros { get; set; } = new Libros();
-	}
+	[ForeignKey("Libros")]
+	public int LibroId { get; set; }
+	public Libros libros { get; set; }
 }
