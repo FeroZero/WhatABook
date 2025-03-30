554bc8654b15d6716ca9c7fe58fd3ae68980d4f1
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-30 12:41:57 -0400

    Modificacion del contexto en el modeling creating.

--- WhatABook/Data/ApplicationDbContext.cs
+++ WhatABook/Data/ApplicationDbContext.cs
@@ -2,23 +2,44 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WhatABook.Models;
 
-namespace WhatABook.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-		public DbSet<Libros> Libros { get; set; }
-		public DbSet<Generos> Generos { get; set; }
-		public DbSet<Ordenes> Ordenes { get; set; }
-		public DbSet<Compras> Compras { get; set; }
-		public DbSet<OrdenesDetalle> OrdenesDetalle { get; set; }
-		public DbSet<GenerosDetalle> GenerosDetalle { get; set; }
+namespace WhatABook.Data;
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.Entity<Compras>()
-				.HasOne(c => c.Libros)  // Una compra tiene un libro
-				.WithOne(l => l.Compras) // Un libro tiene una compra
-				.HasForeignKey<Compras>(c => c.LibroId); // Clave foránea en Compras
-		}
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options)
+	{
+	}
+
+	public DbSet<Libros> Libros { get; set; }
+	public DbSet<Generos> Generos { get; set; }
+	public DbSet<Ordenes> Ordenes { get; set; }
+	public DbSet<Compras> Compras { get; set; }
+	public DbSet<OrdenesDetalle> OrdenesDetalle { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<Libros>()
+		.HasOne(l => l.Compras)
+		.WithOne(c => c.Libros)
+		.HasForeignKey<Compras>(c => c.LibroId);
+
+		modelBuilder.Entity<Generos>().HasData(
+			new Generos { GeneroId = 1, TipoGeneros = "Accion"},
+			new Generos { GeneroId = 2, TipoGeneros = "Drama"},
+			new Generos { GeneroId = 3, TipoGeneros = "Misterio"},
+			new Generos { GeneroId = 4, TipoGeneros = "Fantasia"},
+			new Generos { GeneroId = 5, TipoGeneros = "Romantica"}
+			);
+
+		modelBuilder.Entity<Libros>().HasData(
+			new Libros { LibroId = 1, Autores = "mango", Descripcion = "hola", Titulo = "Muerto", ImagenUrl = "Cosa", GeneroId = 1, FechaPublicacion = new DateTime(2004, 3, 2)}
+			);
+
+		modelBuilder.Entity<Compras>().HasData(
+			new Compras { LibroId = 1, LibroNombre = "Julio Ceasar", Cantidad = 40, Costo = 400, PorcentajeGanancia = 33}
+			);
 	}
 }
