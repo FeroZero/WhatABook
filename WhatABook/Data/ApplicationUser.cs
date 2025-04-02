077219951340a0c0b8b731ccff53d7a8f671e9c1
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-04-02 01:00:30 -0400

    Agregue la capacidad de los atributos ser nuleables temporalmente.

--- WhatABook/Data/ApplicationUser.cs
+++ WhatABook/Data/ApplicationUser.cs
@@ -6,11 +6,11 @@ namespace WhatABook.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-		public string Nombre { get; set; }
+		public string? Nombre { get; set; }
 
-        public int Edad { get; set; }
+        public int? Edad { get; set; }
 
-		public string Direccion { get; set; }
+		public string? Direccion { get; set; }
 
 	}
 
