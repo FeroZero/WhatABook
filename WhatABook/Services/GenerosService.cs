9192c53c008449ffbd7627777f30fa465af15cc1
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-04-02 21:38:09 -0400

    Se creo un Servicio para Generos.

---
+++ WhatABook/Services/GenerosService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WhatABook.Data;
+using WhatABook.Models;
+
+namespace WhatABook.Services;
+
+public class GenerosService(IDbContextFactory<ApplicationDbContext> DbFactory)
+{
+	public async Task<List<Generos>> Listar(Expression<Func<Generos, bool>> criterio)
+	{
+		await using var contexto = await DbFactory.CreateDbContextAsync();
+		return await contexto.Generos
+			.Where(criterio)
+			.ToListAsync();
+	}
+
+	public async Task<Generos?> Buscar(int id)
+	{
+		await using var contexo = await DbFactory.CreateDbContextAsync();
+		return await contexo.Generos
+			.FirstOrDefaultAsync(p => p.GeneroId == id);
+	}
+}
