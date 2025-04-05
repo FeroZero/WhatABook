f0f59c7dedf673548962a30281af0bcb18dd9abe
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-04-05 15:27:22 -0400

    Se creo el modelo metododepago

---
+++ WhatABook/Services/MetodoDePagoServices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WhatABook.Data;
+using WhatABook.Models;
+
+namespace WhatABook.Services
+{
+	public class MetodoDePagoServices(IDbContextFactory<ApplicationDbContext> DbFactory)
+	{
+		public async Task<List<MetodosDePagos>> Listar(Expression<Func<MetodosDePagos, bool>> criterio)
+		{
+			await using var contexto = await DbFactory.CreateDbContextAsync();
+			return await contexto.MetodosDePagos
+				.Where(criterio)
+				.ToListAsync();
+		}
+	}
+}
