3b51e9dc77370c607a0005f99d102ce3b32c1ecd
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-04-05 15:23:43 -0400

    Detalle de libros creado.

---
+++ WhatABook/Models/DetalleLibroGenero.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhatABook.Models
+{
+	public class DetalleLibroGenero
+	{
+		[Key]
+		public int DetalleLibroGeneroId { get; set; }
+
+		public int LibroId { get; set; }
+
+		public int GeneroId { get; set; }
+
+		[ForeignKey("LibroId")]
+		public Libros? Libro { get; set; }
+
+		[ForeignKey("GeneroId")]
+		public Generos? Genero { get; set; }
+	}
+}
